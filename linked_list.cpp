38fe1f37c826a5d4d0e05137d962d1b348528872
araneoAnicet <araneoanicet@gmail.com>
2020-10-27 13:37:27 +0100

    add linked_list iterator

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,1 +1,32 @@
 #include "linked_list.h"
+
+template<typename T>
+ms_gc::linked_list_iterator<T>::linked_list_iterator(const ms_gc::linked_list_iterator<T>& it) {
+    p(it.p);
+}
+
+template<typename T>
+ms_gc::linked_list_iterator<T>::linked_list_iterator(T* p) {
+    p(p);
+}
+
+template<typename T>
+bool ms_gc::linked_list_iterator<T>::operator!=(ms_gc::linked_list_iterator<T> const& other) const {
+    return p != other.p;
+}
+
+template<typename T>
+bool ms_gc::linked_list_iterator<T>::operator==(ms_gc::linked_list_iterator<T> const& other) const {
+    return p == other.p;
+}
+
+template<typename T>
+typename ms_gc::linked_list_iterator<T>::reference ms_gc::linked_list_iterator<T>::operator*() const {
+    return *p;
+}
+
+template<typename T>
+ms_gc::linked_list_iterator<T> &ms_gc::linked_list_iterator<T>::operator++() {
+    ++p;
+    return *this;
+}
--- linked_list.h
+++ linked_list.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <iterator>
+
 
 namespace ms_gc {
     template<class T>
@@ -7,14 +9,27 @@ namespace ms_gc {
         T* next;
     };
 
+    template<typename T>
+    class linked_list_iterator: public std::iterator<std::input_iterator_tag, T> {
+        public:
+            linked_list_iterator(const linked_list_iterator& it);
+            linked_list_iterator(T* p);
+
+            bool operator!=(linked_list_iterator const& other) const;
+            bool operator==(linked_list_iterator const& other) const;
+            typename linked_list_iterator::reference operator*() const;
+            linked_list_iterator& operator++();
+    };
+
 
     template<class T>
     class linked_list {
         private:
             linked_list_node<T>* root_node;
-
+            linked_list_node<T>* top;
         public:
-            
+
             linked_list();
+            linked_list(T* initial_value);
     };
 };
