09b098c0574eae4822652ee4a933d721a85151eb
araneoAnicet <araneoanicet@gmail.com>
2020-10-29 14:16:28 +0100

    add collectables

---
+++ collectable.cpp
@@ -0,0 +1,21 @@
+#include "collectable.h"
+
+void ms_gc::collectable::mark() {
+    this->marked = true;
+}
+
+void ms_gc::collectable::init_collectable() {
+    ms_gc::garbage_collector::get_instance()->add_node(this);
+}
+
+void ms_gc::collectable::remove() {
+    ms_gc::garbage_collector::get_instance()->notify_delete(this);
+}
+
+void ms_gc::collectable::point_to(ms_gc::collectable* other) {
+    this->pointing_to.push(other);
+}
+
+bool ms_gc::collectable::points_to_nodes() {
+    return this->pointing_to.has_elements();
+}
---
+++ collectable.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "linked_list_impl.h"
+#include "garbage_collector.h"
+
+
+namespace ms_gc {
+    class garbage_collector;
+    class collectable {
+        friend garbage_collector;
+        protected:
+            bool marked = false;
+            linked_list<collectable*> pointing_to;
+            void mark();
+            void init_collectable();
+        public:
+            void remove();
+            void point_to(collectable* other);
+            bool points_to_nodes();
+    };
+}
